687b3dfbe335de341064e561e1c39d4403e3adae
MATENPAG <matenpag@gmail.com>
2025-04-26 09:54:56 +0100

    Text, Dialogue & Inventory
    Changes:
    Text/Dialogue
    - You can now interact with some more objects
    - Supports text with multiple lines
    
    Inventory
    - Bag button opens up an inventory panel
    - You can pick up items and put them in your inventory (without your consent)
    - Stored items are shown in the panel
    - Arrow to select object (needs more work but I'm running out of battery)
    
    Other
    - No longer says room name it was very irritating after a while

--- Pink Clouds/Game.py
+++ Pink Clouds/Game.py
@@ -9,6 +9,7 @@ pygame.display.set_caption('Pink Clouds')
 Icon = pygame.image.load("Icon.png")
 pygame.display.set_icon(Icon)
 #variables for background variables
+text_font = pygame.font.Font("PixelifySans-Regular.ttf",24)
 bgcolour = (115,170,250) 
 running  = True 
 pressed = False
@@ -83,6 +84,16 @@ BAG_down = pygame.transform.scale(BAG_down, (96,96))
 BAG_down_rect = pygame.rect.Rect(1,30,96,28)
 showBAG = False
 showBAG_down = False
+inventory_back = pygame.Surface((128,512)) #0,0
+inventory_back.fill((0,0,0))
+showInventory = False
+inventory = []
+inventory_text = ""
+inventory_text_display = text_font.render(inventory_text, True,(255,255,255))
+inventory_text_y = 64
+arrow = pygame.image.load("Arrow.png")
+arrow = pygame.transform.scale(arrow, (32,32))
+arrow_y = 32
 
 Help = pygame.image.load("Help.png")
 Help = pygame.transform.scale(Help, (96,96))
@@ -93,6 +104,13 @@ Help_down_rect = pygame.rect.Rect(1, 59,96,28)
 ShowHelp = False
 ShowHelp_down = False
 
+EXIT = pygame.image.load("Exit.PNG")
+EXIT = pygame.transform.scale(EXIT,(96,96))
+EXIT_rect = pygame.rect.Rect(1,1,96,28)
+EXIT_down = pygame.image.load("Exit Down.PNG")
+EXIT_down = pygame.transform.scale(EXIT_down,(96,96))
+showEXIT = False
+showEXIT_down = False
 #misc
 diary = pygame.image.load("Diary.PNG")
 diary = pygame.transform.scale(diary, (128,128))
@@ -101,16 +119,33 @@ showDiary = True
 BOOK = pygame.image.load("BOOK.PNG")
 BOOK = pygame.transform.scale(BOOK, (512,512))
 showBOOK = False
+diaryText = ""
+diaryText_display = text_font.render(diaryText,True,(0,0,0)).convert_alpha()
+
+knife = pygame.image.load("knife.png")
+knife = pygame.transform.scale(knife, (64,64))
+showKnife = False
+knife_inventory = "knife"
+showKnife_inventory = False
 
 #dialogue
-text_font = pygame.font.Font("PixelfySans-Regular.ttf")
+text_full = ""
 text = ""
 text_display = text_font.render(text,True,(255,255,255))
-text_rect = pygame.rect.Rect(
+text_rect = text_display.get_rect(topleft = (6,400))
+text_count = 0
+text_timer = 0
+text_speed = 20
+text_y = 400
+line1 = ""
+line2 = ""
+line3 = ""
+
 
 description = pygame.Surface((504,108), pygame.SRCALPHA) #4,400 x,y
 description.fill((55,55,55,127))
 showDescription = False
+box = ""
 
 #TRYING OUT CLASSESS
 
@@ -214,10 +249,12 @@ class Player(pygame.sprite.Sprite):
             desk_rect = pygame.Rect(85,180-TOP_OF_PLAYER,215-LEFT_OF_PLAYER,100)#desk
             bed_rect = pygame.Rect(0-LEFT_OF_PLAYER,180-TOP_OF_PLAYER,120,180)
             bedDoor_rect = pygame.Rect(320-LEFT_OF_PLAYER,90,150,180)#SUBTRACT BC LEFT#SUBTRACT 90 BC UP
-                    
+            plant_rect = pygame.rect.Rect(450 + LEFT_OF_PLAYER,440,200,200)        
             if desk_rect.collidepoint(herex, herey): 
                 return #dont move at all (dont run 'move' anymore once)
             elif bed_rect.collidepoint(herex,herey):#top right
+                return
+            elif plant_rect.collidepoint(herex + 128, herey +128):
                 return
 
             if bedDoor_rect.collidepoint(herex,herey):# top left
@@ -349,18 +386,53 @@ class Dot():
             self.current = 0
     def interact(self):
         global mouse, showBedroom, showHallway1, showHallway2, showKitchen, interact
-        if showBedroom:
-            plant_rect = pygame.rect.Rect(435,430,512-436,512-431)
-            diary_rect = pygame.rect.Rect(132,150,128,128)
-            if plant_rect.collidepoint(mouse):
-                self.show = True
-                interact = "plant pot"
-            elif diary_rect.collidepoint(mouse):
-                self.show = True
-                interact = "diary"
+        global showBOOK
+        if not showBOOK:
+            if showBedroom:
+                plant_rect = pygame.rect.Rect(435,430,512-436,512-431)
+                diary_rect = pygame.rect.Rect(160,190,80,50)
+                if plant_rect.collidepoint(mouse):
+                    self.show = True
+                    interact = "plant pot"
+                elif diary_rect.collidepoint(mouse):
+                    self.show = True
+                    interact = "diary"
+                else:
+                    self.show = False
+                    interact = ""
+            elif showHallway1:
+                painting_rect = pygame.rect.Rect(256,70,144,70)
+                if painting_rect.collidepoint(mouse):
+                    self.show = True
+                    interact = "painting_1"
+                else:
+                    self.show = False
+                    interact = ""
+            elif showHallway2:
+                painting_rect = pygame.rect.Rect(120,70,144,70)
+                if painting_rect.collidepoint(mouse):
+                    self.show = True
+                    interact = "painting_2"
+                else:
+                    self.show = False
+                    interact = ""
+            elif showKitchen:
+                fridgeNote_rect = pygame.rect.Rect(40,170,55,50)
+                knife_rect = pygame.rect.Rect(315,180,30,30)
+                if fridgeNote_rect.collidepoint(mouse):
+                    self.show = True
+                    interact = "fridge note"
+                elif knife_rect.collidepoint(mouse):
+                    self.show = True
+                    interact = "knife"
+                else:
+                    self.show = False
+                    interact = ""
             else:
                 self.show = False
                 interact = ""
+        else:
+            self.show = False
         
     def draw(self,screen,mouse):
         if self.show:
@@ -368,6 +440,21 @@ class Dot():
             screen.blit(current_frame, (mouse[0]-32,mouse[1]-24))
 
 dot = Dot()        
+def resetText():
+    global text,text_count,text_y,current_line, line1,line2,line3, showDescription, showSpace
+    text = ""
+    text_count = 0
+    text_y = 400
+    current_line = 0
+    line1 = ""
+    line2 = ""
+    line3 = ""
+    showDescription = True
+    showSpace = True
+
+def loadAllMisc():
+    global showKnife
+    showKnife = True
     
 #this is the main loop
 while running: 
@@ -393,11 +480,12 @@ while running:
                     showHELP = False
                     showMENU = True
                     showRoomName = True
+                    loadAllMisc()
                 if showDescription:
-                    if interacted == "plant pot":
-                        showDescription = False
-                        showSpace = False
-                        interacted = ""
+                    showDescription = False
+                    showSpace = False
+                    interacted = ""
+                    text_full = ""
                 if interacted == "diary":
                     showBOOK = False
                     showSpace = False
@@ -419,34 +507,66 @@ while running:
                     showHallway1 = False
                     hallwayBed = False
                 elif hallwayKitchen and showHallway2:
-                    player.x,player.y = 384,300
+                    player.x,player.y = 320,350
                     roomName = "KITCHEN"
                     showRoomName = True
                     showKitchen = True
                     showHallway2 = False
                     hallwayKitchen = False
                 elif kitchenDoor and showKitchen:
-                    player.x,player.y = 390,290
+                    player.x,player.y = 340,120 #390,290 to enter house
                     roomName = "HALLWAY"
                     showRoomName = True
                     showHallway2 = True
                     showKitchen = False
                     kitchenDoor = False
+        elif event.type == pygame.K_DOWN or event.type == pygame.K_LEFT:
+            print("D")
+            arrow_y += text_font.get_height() + 4
+        elif event.type == pygame.K_UP:
+            arrow_y -= text_font.get_height() + 4
         elif event.type == pygame.MOUSEBUTTONDOWN:
             print(mouse)
             if showMENU_down:
                 MENU_rect = pygame.rect.Rect(1,1,96,116)
                 MENU_down_RECT = pygame.rect.Rect(1,1,96,28)
                 showBAG = True
+            if showBAG_down:
+                showInventory = True
+            if showEXIT_down:
+                showInventory = False
+                showEXIT = False
+                showEXIT_down = False
             if interact == "plant pot":
-                showDescription = True
-                showSpace = True
+                box = "grey"
+                text_full = "An oriental lily, obviously you can't tell. " #have to put space after i think
+                resetText()
                 interacted = "plant pot"
             elif interact == "diary":
                 showBOOK = True
                 interacted = "diary"
                 showSpace = True
-
+            elif interact == "painting_1":
+                box = "grey"
+                text_full = "A picture of the mountains we went to. "
+                resetText()
+                interacted = "painting_1"
+            elif interact == "painting_2":
+                box = "grey"
+                text_full = "An old painting I made in primary school. I|wish Mum would take it down."
+                resetText()  
+                interacted = "painting_2"
+            elif interact == "fridge note":
+                box = "grey"
+                text_full = "A heartfelt letter from someone. The|sender's name is blocked out."
+                resetText()  
+                interacted = "fridge note"
+            elif interact == "knife":
+                box = "grey"
+                text_full = "A kitchen knife. Nothing more to it."
+                resetText()  
+                interacted = "knife"
+                showKnife = False
                                     
     if MENU_rect.collidepoint(mouse) and not showthumbnail and not showHELP: #event.pos get position of event (mouse)
         showMENU_down = True
@@ -461,6 +581,11 @@ while running:
         showBAG_down = True
     else:
         showBAG_down = False
+
+    if EXIT_rect.collidepoint(mouse) and showEXIT:
+        showEXIT_down = True
+    else:
+        showEXIT_down = False
     #if MENU_rect.collidepoint(mouse):
         #showMENU_down = True
 
@@ -506,22 +631,86 @@ while running:
             screen.blit(hallway2,(0,0))
         if showKitchen:
             screen.blit(kitchen, (0,0))
+            if showKnife:
+                showKnife_inventory = False
+                screen.blit(knife, (300,168))
+            else:
+                showKnife_inventory = True
+
+        #adding items to inventory
+        if showKnife_inventory:
+            showKnife = False
+            if "knife" not in inventory:
+                inventory.append(knife_inventory)
 
         #CHARACTER STUFF????
         if showHELP == False and showthumbnail == False:
             player.update()
             player.draw(screen) 
-        #buttons
+        #buttons and description
         if showE:
             screen.blit(E,(0,height - 64))
-        if showDescription:
-            screen.blit(description, (4,400))
-        if showSpace:
-            screen.blit(space, space_rect)
             
         #DOOOOOOR STUFF????
         door.setOpacity(player)
         door.draw(screen)
+        if showBAG:
+            screen.blit(BAG, (0,0))
+        if showBAG_down:
+            screen.blit(BAG_down, (0,0))
+        if showInventory:
+            screen.blit(inventory_back,(0,0))
+            screen.blit(arrow, (96,arrow_y))
+            inventory_text_y = 32
+            showEXIT = True
+            for item in inventory:
+                inventory_text = item
+                inventory_text_display = text_font.render(inventory_text,True,(255,255,255))
+                screen.blit(inventory_text_display,(0,inventory_text_y))
+                inventory_text_y += text_font.get_height() + 4
+
+                
+        if showDescription: #REMEMBER AS LONG AS THIS IS TRUE IT STAYS LIEK THIS
+            if box == "grey":
+                screen.blit(description, (4,400))
+            lines = text_full.split("|")
+            line1 = lines[0] if len(lines) >= 1 else ""
+            line2 = lines[1] if len(lines) >= 2 else ""
+            line3 = lines[2] if len(lines) >= 3 else ""
+
+            # Always blit finished lines
+            if current_line >= 1:
+                screen.blit(text_font.render(line1, True, (255, 255, 255)), (text_rect[0], text_y))
+            if current_line >= 2:
+                screen.blit(text_font.render(line2, True, (255, 255, 255)), (text_rect[0], text_y + text_font.get_height() + 4))
+
+            if current_line == 0:
+                line = line1
+            elif current_line == 1:
+                line = line2
+            elif current_line == 2:
+                line = line3
+            else:
+                line = ""
+                
+            if text_count < len(line):
+                if now - text_timer > text_speed:
+                    current_character = line[text_count]
+                    text_count += 1
+                    text = line[:text_count]
+                    text_display = text_font.render(text, True, (255, 255, 255))
+                    if current_character in [",", "."]:
+                        text_timer = now + 200
+                    else:
+                        text_timer = now
+
+                y_offset = current_line * (text_font.get_height() + 4)
+                screen.blit(text_display, (text_rect[0], text_y + y_offset))
+            else:
+                current_line += 1
+                text_count = 0
+        if showSpace:
+            screen.blit(space, space_rect)
 
         #... STUFF????
         dot.update()
@@ -531,32 +720,32 @@ while running:
             screen.blit(BOOK, (0,0,))
     #if i put black here will it pritn over player and door??
 
-#FADE THE ROOM NAME BROO WHY IS THIS SO LONG
-    if showRoomName:
-        roomName_opacity = 255
-        roomName_font = pygame.font.Font("Lexend-SemiBold.ttf",width//8)
-        roomName_display = roomName_font.render(roomName, True, (255,255,255)).convert_alpha()#NEED THIS FOR NON IMAGES
-        roomName_rect = roomName_display.get_rect(center = (width//2,height//2))
-        fadeRoomName = True
-        showRoomName = False
+#FADE THE ROOM NAME BROO WHY IS THIS SO LONG JUST TO SCRAP IN THE END
+    #if showRoomName:
+        #roomName_opacity = 255
+        #roomName_font = pygame.font.Font("Lexend-SemiBold.ttf",width//8)
+        #roomName_display = roomName_font.render(roomName, True, (255,255,255)).convert_alpha()#NEED THIS FOR NON IMAGES
+        #roomName_rect = roomName_display.get_rect(center = (width//2,height//2))
+        #fadeRoomName = True
+        #showRoomName = False
 
-    if fadeRoomName:
-        if roomName_opacity > 0:
-            roomName_opacity -= 3
-            roomName_display.set_alpha(roomName_opacity)
-        else:
-            fadeRoomName = False
-    screen.blit(roomName_display,roomName_rect)
+    #if fadeRoomName:
+        #if roomName_opacity > 0:
+            #roomName_opacity -= 3
+            #roomName_display.set_alpha(roomName_opacity)
+        #else:
+            #fadeRoomName = False
+    #screen.blit(roomName_display,roomName_rect)
 
 #menu buitl different
     if showMENU:
         screen.blit(MENU, (0,-32))
     if showMENU_down:
         screen.blit(MENU_down, (0,-32))
-    if showBAG:
-        screen.blit(BAG, (0,0))
-    if showBAG_down:
-        screen.blit(BAG_down, (0,0))
+    if showEXIT:
+        screen.blit(EXIT,(0,-32))
+    if showEXIT_down:
+        screen.blit(EXIT_down,(0,-32))
     #should stay at bottom
     clock.tick(60)
     pygame.display.flip()
